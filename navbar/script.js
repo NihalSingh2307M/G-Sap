188fb1a7927a5161123021d42e29f0e035665b53
NihalSingh <nihalsingh2307n@gmail.com>
2025-06-05 20:17:46 +0530

    navbar

---
+++ navbar/script.js
@@ -0,0 +1,19 @@
+var tl = gsap.timeline()
+tl.from("h2",{
+    y:-30,
+    opacity:0,
+    duration:1,
+    delay:0.5
+})
+tl.from("h4",{
+    y:-30,
+    duration:1,
+    opacity:0,
+    stagger:0.3
+})
+tl.from("#mid",{
+    y:20,
+    opacity:0,
+    duration:0.5,
+    scale:0.2
+})
